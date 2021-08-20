d765dbe8616554619acc4e9ffea24824f2b90ce5
Denis Solovyov <infinityx93@gmail.com>
2021-08-20 14:31:06 +0300

    minimize

--- app/src/main/java/ru/skillbox/custom_dialog_interface_tutorial/ExampleDialog.kt
+++ app/src/main/java/ru/skillbox/custom_dialog_interface_tutorial/ExampleDialog.kt
@@ -2,7 +2,6 @@ package ru.skillbox.custom_dialog_interface_tutorial
 
 import android.app.Dialog
 import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -11,27 +10,27 @@ import androidx.appcompat.app.AppCompatDialogFragment
 
 
 class ExampleDialog : AppCompatDialogFragment() {
-    private var editTextUsername: EditText? = null
-    private var editTextPassword: EditText? = null
     private var listener: ExampleDialogListener? = null
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        //val builder: AlertDialog.Builder = android.app.AlertDialog.Builder(activity)
-        val builder: AlertDialog.Builder = androidx.appcompat.app.AlertDialog.Builder(requireActivity())
         val inflater = requireActivity().layoutInflater
         val view: View = inflater.inflate(R.layout.layout_dialog, null)
-        builder.setView(view)
+
+        val editTextUsername = view.findViewById<EditText>(R.id.edit_username)
+        val editTextPassword = view.findViewById<EditText>(R.id.edit_password)
+
+        return AlertDialog.Builder(requireActivity())
+            .setView(view)
             .setTitle("Login")
-            .setNegativeButton("cancel",
-                DialogInterface.OnClickListener { dialogInterface, i -> })
-            .setPositiveButton("ok",
-                DialogInterface.OnClickListener { dialogInterface, i ->
-                    val username = editTextUsername!!.text.toString()
-                    val password = editTextPassword!!.text.toString()
-                    listener!!.applyTexts(username, password)
-                })
-        editTextUsername = view.findViewById(R.id.edit_username)
-        editTextPassword = view.findViewById(R.id.edit_password)
-        return builder.create()
+            .setNegativeButton("Cancel") {_, _, ->}
+            .setPositiveButton("Ok") {_, _, ->
+                val username = editTextUsername.text.toString()
+                val password = editTextPassword.text.toString()
+                listener!!.applyTexts(
+                    editTextUsername.text.toString(),
+                    editTextPassword.text.toString()
+                )
+            }
+            .create()
     }
 
     override fun onAttach(context: Context) {
--- app/src/main/java/ru/skillbox/custom_dialog_interface_tutorial/MainActivity.kt
+++ app/src/main/java/ru/skillbox/custom_dialog_interface_tutorial/MainActivity.kt
@@ -1,28 +1,28 @@
 package ru.skillbox.custom_dialog_interface_tutorial
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), ExampleDialog.ExampleDialogListener {
     private var textViewUsername: TextView? = null
     private var textViewPassword: TextView? = null
-    private var button: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textViewUsername = findViewById<View>(R.id.textview_username) as TextView
-        textViewPassword = findViewById<View>(R.id.textview_password) as TextView
-        button = findViewById<View>(R.id.button) as Button
-        button!!.setOnClickListener { openDialog() }
-        /*button.setOnClickListener(object : View.OnClickListener() {
-            fun onClick(v: View?) {
-                openDialog()
-            }
-        })*/
+        textViewUsername = textview_username
+        textViewPassword = textview_password
+
+        //You can use the old search method
+        //val button = findViewById<View>(R.id.button) as Button
+
+        //Or use the synthetic library. Don't forget to add
+        //'kotlin-android-extensions' plugin in gradle
+        button.setOnClickListener { openDialog() }
+
     }
 
     private fun openDialog() {
